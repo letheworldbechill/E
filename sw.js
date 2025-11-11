298b2bc51a173fd2be3b390a9d9eb5d36708d599
Severin Leander <gasserseverin1@gmail.com>
2025-11-11 20:01:54 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,19 @@
-self.addEventListener('install',e=>e.waitUntil(caches.open('s').then(c=>c.addAll(['./','./index.html','./style.css','./script.js','./manifest.json','./icon_sevu_emdr.png','./safe_heartbeat.wav']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('install', e => 
+  e.waitUntil(caches.open('emdr-cache').then(c => 
+    c.addAll([
+      './',
+      './index.html',
+      './style.css',
+      './script.js',
+      './manifest.json',
+      './safe_heartbeat.wav',
+      './mountains.png',
+      './forest.png',
+      './water.png'
+    ])
+  ))
+);
+
+self.addEventListener('fetch', e => 
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
+);
